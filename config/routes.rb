37e9f56af03b5cbf4ec2fb8f630efe6e147860ff
Sergey Monin <moninsergei@gmail.com>
2015-09-06 21:32:37 +0400

    Small fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  concern :paginatable do
+    get '(page/:page)', :action => :index, :on => :collection, :as => ''
+  end
+
+  resources :posts, :concerns => :paginatable
   resources :posts do
     resources :comments
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904002715) do
+ActiveRecord::Schema.define(version: 20150906050621) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
