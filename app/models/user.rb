acf0a2538802ad80945ee5034e532139d70e6cb8
Sergey Monin <moninsergei@gmail.com>
2015-11-21 21:01:38 +0300

    Stylistical changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,24 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
 
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
   validates :username,
-            :presence => true,
-            :length => { minimum: 4 },
-            :uniqueness => {
-                :case_sensitive => false
+            presence: true,
+            length: { minimum: 4 },
+            uniqueness: {
+                case_sensitive: false
             }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
-            :presence => true,
-            :format => { with: VALID_EMAIL_REGEX },
-            :uniqueness => {
+            presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: {
                 case_sensitive: false
             }
 
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
     else
       where(conditions.to_hash).first
     end
