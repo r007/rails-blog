15a61b1082518cd24ccdf1136f6b68e5a56e536c
Sergey Monin <moninsergei@gmail.com>
2015-11-21 20:55:41 +0300

    Stylistical changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   concern :paginatable do
-    get '(page/:page)', :action => :index, :on => :collection, :as => ''
+    get '(page/:page)', action: :index, on: :collection, as: ''
   end
 
-  resources :posts, :concerns => :paginatable
+  resources :posts, concerns: :paginatable
   resources :posts do
     resources :comments
   end
-  root "posts#index"
+  root 'posts#index'
 
   get '/about', to: 'pages#about'
 end
